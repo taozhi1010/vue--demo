5858178bcfd7139ada333aa470cf988470fa7e1e
taozhi <805446529@qq.com>
2018-10-18 10:47:29 +0800

    引入了mint-ui

---
+++ vue-study/src/components/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+import { Toast } from 'mint-ui';
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'login"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
